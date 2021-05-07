0c1257eb8bc717c53e5e75bf759990457b0a5a0a
version07 <version07@naver.com>
2021-05-07 17:51:09 +0900

    change a little bit

---
+++ JavaEx/src/com/javaex/problem02/Friend.java
@@ -0,0 +1,10 @@
+package com.javaex.problem02;
+
+public class Friend {
+	private String information;
+	
+	public Friend(String information){
+		this.information = information;
+
+}
+}
---
+++ JavaEx/src/com/javaex/problem02/FriendApp.java
@@ -0,0 +1,14 @@
+package com.javaex.problem02;
+
+public class FriendApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Friend[] friends = new Friend[3];
+		
+		
+		
+
+	}
+
+}
